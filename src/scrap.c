ab3f5bf8bf49b75817094ec90650ffd1f19dc419
Lina Torovoltas <193523522+lina-torovoltas@users.noreply.github.com>
2025-01-27 15:08:34 +0500

    Comments written

--- src/scrap.c
+++ src/scrap.c
@@ -22,6 +22,7 @@
 
 #include <math.h>
 
+// Global Variables
 Image logo_img;
 
 Shader line_shader;
@@ -85,6 +86,7 @@ double ui_time = 0.0;
 float shader_time = 0.0;
 int blockchain_select_counter = -1;
 
+// Vertex shader for drawing lines
 const char* line_shader_vertex =
     "#version 330\n"
     "in vec3 vertexPosition;\n"
@@ -99,6 +101,7 @@ const char* line_shader_vertex =
     "    gl_Position = pos;\n"
     "}";
 
+// Fragment shader for drawing lines
 const char* line_shader_fragment =
     "#version 330\n"
     "in vec2 fragCoord;\n"
@@ -113,17 +116,20 @@ const char* line_shader_fragment =
     "    finalColor = vec4(fragColor.xyz, pow(diff, 2.0));\n"
     "}";
 
+// Text for top bar buttons
 char* top_bar_buttons_text[3] = {
     "File",
     "Settings",
     "About",
 };
 
+// Text for tab bar buttons
 char* tab_bar_buttons_text[2] = {
     "Code",
     "Output",
 };
 
+// Recursively checks nested blocks for correct structure and connection with the parent block
 void sanitize_block(ScrBlock* block) {
     for (vec_size_t i = 0; i < vector_size(block->arguments); i++) {
         if (block->arguments[i].type != ARGUMENT_BLOCK) continue;
@@ -136,6 +142,7 @@ void sanitize_block(ScrBlock* block) {
     }
 }
 
+// Checks the integrity and correctness of connections of all blocks of editor code and the mouse blockchain
 void sanitize_links(void) {
     for (vec_size_t i = 0; i < vector_size(editor_code); i++) {
         ScrBlock* blocks = editor_code[i].blocks;
@@ -149,6 +156,7 @@ void sanitize_links(void) {
     }
 }
 
+// Loads an SVG image from a file and converts it to a texture with bilinear filtering
 Texture2D load_svg(const char* path) {
     Image svg_img = LoadImageSvg(path, conf.font_size, conf.font_size);
     Texture2D texture = LoadTextureFromImage(svg_img);
@@ -157,15 +165,18 @@ Texture2D load_svg(const char* path) {
     return texture;
 }
 
+// Returns the absolute path to the font, converting the relative path to a path inside the data directory
 const char* get_font_path(char* font_path) {
     return font_path[0] != '/' && font_path[1] != ':' ? into_data_path(font_path) : font_path;
 }
 
+// Calculates the dimensions of a GUI element based on the image texture and the given height size
 GuiMeasurement scrap_gui_measure_image(void* image, unsigned short size) {
     Texture2D* img = image;
     return (GuiMeasurement) { img->width * ((float)size / (float)img->height), size };
 }
 
+// Looks up the glyph index for the given character (code point) or returns the fallback glyph index ('?') if there are no matches
 int search_glyph(int codepoint) {
     // We assume that ASCII region is the first region, so this index should correspond to char '?' in the glyph table
     const int fallback = 31;
@@ -176,6 +187,7 @@ int search_glyph(int codepoint) {
     return fallback;
 }
 
+// Calculates text sizes for a given font and font size
 GuiMeasurement custom_measure(Font font, const char *text, float font_size) {
     GuiMeasurement ms = {0};
 
@@ -203,10 +215,12 @@ GuiMeasurement custom_measure(Font font, const char *text, float font_size) {
     return ms;
 }
 
+// Wrapper for calling custom_measure with passing font and text parameters
 GuiMeasurement scrap_gui_measure_text(void* font, const char* text, unsigned short size) {
     return custom_measure(*(Font*)font, text, size);
 }
 
+// Divides the panel into two parts along the specified side with the specified split percentage
 void panel_split(PanelTree* panel, SplitSide side, PanelType new_panel_type, float split_percent) {
     if (panel->type == PANEL_SPLIT) return;
 
@@ -258,6 +272,7 @@ void panel_split(PanelTree* panel, SplitSide side, PanelType new_panel_type, flo
     }
 }
 
+// Creates a new panel object with the given type and initializes its parameters
 PanelTree* panel_new(PanelType type) {
     PanelTree* panel = malloc(sizeof(PanelTree));
     panel->type = type;
@@ -267,6 +282,7 @@ PanelTree* panel_new(PanelType type) {
     return panel;
 }
 
+// Removes a panel and its child panels recursively, freeing memory
 void panel_delete(PanelTree* panel) {
     assert(panel != NULL);
 
@@ -281,10 +297,21 @@ void panel_delete(PanelTree* panel) {
     free(panel);
 }
 
+// Removes a tab by index and frees its resources
+void tab_delete(size_t tab) {
+    assert(tab < vector_size(code_tabs));
+    panel_delete(code_tabs[tab].root_panel);
+    vector_free(code_tabs[tab].name);
+    vector_remove(code_tabs, tab);
+    if (current_tab >= (int)vector_size(code_tabs)) current_tab = vector_size(code_tabs) - 1;
+}
+
+//Deletes all tabs starting with the last one using the tab_delete function
 void delete_all_tabs(void) {
     for (ssize_t i = vector_size(code_tabs) - 1; i >= 0; i--) tab_delete(i);
 }
 
+// Creates a new tab with the given name and panel, adding it to the list of tabs
 size_t tab_new(char* name, PanelTree* root_panel) {
     if (!root_panel) {
         TraceLog(LOG_WARNING, "Got root_panel == NULL, not adding");
@@ -300,6 +327,7 @@ size_t tab_new(char* name, PanelTree* root_panel) {
     return vector_size(code_tabs) - 1;
 }
 
+// Inserts a new tab with the given name and panel at the specified position in the list of tabs
 void tab_insert(char* name, PanelTree* root_panel, size_t position) {
     if (!root_panel) {
         TraceLog(LOG_WARNING, "Got root_panel == NULL, not adding");
@@ -313,14 +341,7 @@ void tab_insert(char* name, PanelTree* root_panel, size_t position) {
     tab->root_panel = root_panel;
 }
 
-void tab_delete(size_t tab) {
-    assert(tab < vector_size(code_tabs));
-    panel_delete(code_tabs[tab].root_panel);
-    vector_free(code_tabs[tab].name);
-    vector_remove(code_tabs, tab);
-    if (current_tab >= (int)vector_size(code_tabs)) current_tab = vector_size(code_tabs) - 1;
-}
-
+// Initializes panels, creating and splitting a code panel, then adding tabs for code and output
 void init_panels(void) {
     PanelTree* code_panel = panel_new(PANEL_CODE);
     panel_split(code_panel, SPLIT_SIDE_LEFT, PANEL_SIDEBAR, 0.3);
@@ -328,6 +349,7 @@ void init_panels(void) {
     tab_new("Output", panel_new(PANEL_TERM));
 }
 
+// Initializes resources and settings by loading textures, fonts, and configurations, and sets up GUI and panel interface
 void setup(void) {
     render_surface = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
     SetTextureWrap(render_surface.texture, TEXTURE_WRAP_MIRROR_REPEAT);
@@ -399,6 +421,8 @@ void setup(void) {
     init_gui_window();
 }
 
+
+// Main function: Initializes configurations, sets up window, processes input, renders GUI, and cleans up resources on exit
 int main(void) {
     config_new(&conf);
     config_new(&window_conf);
@@ -471,6 +495,7 @@ int main(void) {
         }
 
         BeginDrawing();
+            // Draws a render_surface texture with a custom rectangle, flipping it horizontally if Arabic mode is enabled
             DrawTexturePro(
                 render_surface.texture, 
 #ifdef ARABIC_MODE
